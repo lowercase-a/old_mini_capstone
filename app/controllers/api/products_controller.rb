45cde8b0d5aece1d81ccf93edff665d7a740a658
Brian <riceb53@gmail.com>
2019-02-26 21:09:06 -0600

    add second product url

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -4,6 +4,11 @@ class Api::ProductsController < ApplicationController
     render 'my_product.json.jbuilder'
   end
 
+  def the_second_product
+    @product = Product.second
+    render 'my_product.json.jbuilder'
+  end
+
   def products
     @products = Product.all
     render 'all_of_the_products.json.jbuilder'
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # verb "/urls" => "namespace/controllers#action"
   namespace :api do
     get '/product' => 'products#one_product'
+    get '/second_product' => 'products#the_second_product'
     get '/all_products' => 'products#products'
   end
 end
