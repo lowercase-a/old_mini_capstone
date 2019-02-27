a8118b42875c7f8a00aaa341d81714f7899ae186
Brian <riceb53@gmail.com>
2019-02-26 21:05:37 -0600

    add routes for all products and one particular product

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,2 +1,11 @@
 class Api::ProductsController < ApplicationController
+  def one_product
+    @product = Product.first
+    render 'my_product.json.jbuilder'
+  end
+
+  def products
+    @products = Product.all
+    render 'all_of_the_products.json.jbuilder'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # STEP 1: A ROUTE triggers a controller action
   # verb "/urls" => "namespace/controllers#action"
+  namespace :api do
+    get '/product' => 'products#one_product'
+    get '/all_products' => 'products#products'
+  end
 end
