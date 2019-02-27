cf97f4b55d0e081ef0d741ec189cb3a13b5ca8af
Brian <riceb53@gmail.com>
2019-02-26 20:41:19 -0600

    make model and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+product1 = Product.new(name: "toothpaste", price: 2, description: "it's like soap, but for your mouth", image_url: "https://target.scene7.com/is/image/Target/GUEST_5c43cd51-03b4-4921-bd47-79c3a08cf60e?wid=488&hei=488&fmt=pjpeg")
+
+product1.save
